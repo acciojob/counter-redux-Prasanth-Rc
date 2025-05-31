a1d9699dde6b96ca7fc73a91249fb62e30cb21c0
Prasanth-Rc <prasanth98780@gmail.com>
2025-05-31 12:51:29 +0530

    Update 2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
+import { connect } from 'react-redux';
+
+const App = ({ count, increment, decrement }) => {
+  return (
+    <div>
+      <h1>{count}</h1>
+      <div>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div>
+    </div>
+  );
+};
 
 // Actions
 const increment = () => ({ type: 'INCREMENT' });
 const decrement = () => ({ type: 'DECREMENT' });
 
-// Reducer
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT': return state + 1;
-    case 'DECREMENT': return state - 1;
-    default: return state;
-  }
+const mapStateToProps = (state) => ({
+  count: state
+});
+
+const mapDispatchToProps = {
+  increment,
+  decrement
 };
 
-// Store
-const store = createStore(counterReducer);
-
-// Counter Component
-const Counter = ({ count, increment, decrement }) => (
-  <div>
-    <h1>{count}</h1>
-    <div>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  </div>
-);
-
-// Connect Component to Redux
-const mapStateToProps = state => ({ count: state });
-const ConnectedCounter = connect(
-  mapStateToProps,
-  { increment, decrement }
-)(Counter);
-
-// App Component
-const App = () => (
-  <Provider store={store}>
-    <ConnectedCounter />
-  </Provider>
-);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store';
-import Counter from './components/Counter';
+import { createStore } from 'redux';
+import App from './App';
 
+// Reducer
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
+// Store
+const store = createStore(counterReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Counter />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
